f5df6c280fef902d154a238452f19ddeb58237c8
Leinerke Suarez <leinerke2000@gmail.com>
2022-02-06 13:01:42 -0500

    Página de curso individual.

---
+++ resources/views/course.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.web')
+
+@section('content')
+    Detaller del curso
+    <div class="text-center">
+        <h1 class="text-3xl text-gray-700 mb-2 uppercase">Ultimos Cursos</h1>
+        <h2 class="text-xl text-gray-600">Formate online como profesional en tecnologia</h2>
+    </div>
+    <livewire:course-list />
+@endsection
