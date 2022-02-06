bc302ff37135f891bbab487b2f06e053933c5004
Leinerke Suarez <leinerke2000@gmail.com>
2022-02-06 13:19:53 -0500

    Listado de lecciones.

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -7,7 +7,18 @@
                 <li class="font-medium text-sm text-gray-400 mb-4">
                     Contenido
                 </li>
-                ---
+                @foreach($course->posts as $post)
+                    <li class="flex items-center text-gray-600 mt-2">
+                        {{ $post->name }}
+                        @if($post->free)
+                            <span
+                                class="text-xs text-gray-500 font-semibold bg-gray-300 rounded-full ml-auto px-2"
+                            >
+                                Gratis
+                            </span>
+                        @endif
+                    </li>
+                @endforeach
             </ul>
         </div>
         <div class="text-gray-700 col-span-2">
